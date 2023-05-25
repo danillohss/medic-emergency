69d60fa475337a99c3cf9e4c633012d4eb22c032
Danillo Soares <danillohss@gmail.com>
2023-05-24 21:59:15 -0300

    Vue avançado, trabalhando com vuex

--- src/components/ConfiguracaoEquipe.vue
+++ src/components/ConfiguracaoEquipe.vue
@@ -7,31 +7,49 @@
     </div>
     <div class="row">
       <div class="col-8">
-        <p>Enfermeiro:</p>
-        <p>Socorrista:</p>
-        <p>Médico:</p>
-        <p>Carro:</p>
-        <p>Telefone:</p>
-        <p>Kit de reanimação:</p>
+        <p>
+          Enfermeiro: <strong>{{ team.nurse }}</strong>
+        </p>
+        <p>
+          Socorrista: <strong>{{ $store.state.team.rescuer }}</strong>
+        </p>
+        <p>
+          Médico: <strong>{{ $store.state.team.doctor }}</strong>
+        </p>
+        <p>
+          Carro: <strong>{{ $store.state.team.ambulance }}</strong>
+        </p>
+        <p>
+          Telefone: <strong>{{ $store.state.team.phone }}</strong>
+        </p>
+        <p>
+          Kit de reanimação:
+          <strong>{{ $store.state.team.reanimationKit }}</strong>
+        </p>
       </div>
       <div class="col-4 text-center">
         <div class="row">
           <div class="col">
-            <img class="img-fluid" :src="require('@/assets/ambulancias/indefinida.png')">
+            <img
+              class="img-fluid"
+              :src="require('@/assets/ambulancias/indefinida.png')"
+            />
           </div>
         </div>
         <div class="row mt-3">
           <div class="col">
             <button type="button" class="btn btn-primary">Montar equipe</button>
           </div>
-        </div>              
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-    name: 'CONFIGURACAOEQUIPE'
-}
+  name: "CONFIGURACAOEQUIPE",
+  computed: mapState(["team"]),
+};
 </script>
--- src/store.js
+++ src/store.js
@@ -1,3 +1,15 @@
 import Vuex from 'vuex';
 
-export default new Vuex.Store({})
+export default new Vuex.Store({
+    state: {
+        titulo: 'Emergências médicas',
+        team: {
+            nurse: 'Nome enfermeiro',
+            rescuer: 'Nome do socorrista',
+            doctor: 'Nome do médico',
+            ambulance: 'Placa da ambulância',
+            phone: '+55 (11) 9 9999-9999',
+            reanimationKit: 'Kit 001'
+        }
+    }
+})
